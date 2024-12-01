06e65d823f7d542fa309ee97979b0c2b8bb875be
Olga Kulczycka <olga@webjaksklep.eu>
2024-12-01 12:44:20 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import App from "./components/app/App";
+import MarvelService from "./services/MarvelService";
+import "./style/style.scss";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
