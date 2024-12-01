8d45227f7be07337d041e95f1f0bae6e1875cb60
Olga Kulczycka <olga@webjaksklep.eu>
2024-12-01 14:53:02 +0100

    dev: random character component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
 import "./style/style.scss";
 
 const container = document.getElementById("root");
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,12 +12,24 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (res) => {
+        return {
+            name: res.name,
+            description: res.description ? `${res.description.slice(0, 210)}...` : 'There is no description for this charakter.',
+            thumbnail: `${res.thumbnail.path}.${res.thumbnail.extension}`,
+            homepage: res.urls[0].url,
+            wiki: res.urls[1].url
+        }
     }
 }
 
